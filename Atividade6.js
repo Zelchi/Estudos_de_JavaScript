a71c5b11242d500328507c21750131de39003e0a
Zelchi <tininhakawaii@gmail.com>
2024-02-27 01:12:44 -0300

    modified:   Atividade6.js 	new file:   Aula10.js

--- Atividade6.js
+++ Atividade6.js
@@ -1,1 +1,59 @@
-// https://cursos.alura.com.br/course/javascript-conhecendo-arrays/task/148946
+// 1
+
+const array1 = [1, 2, 3];
+const array2 = [4, 5, 6];
+
+const array3 = array1.concat(array2);
+
+console.log(array3);
+
+// 2
+
+const array4 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const array5 = array4.slice(3, 8);
+
+console.log(array5);
+
+// 3
+
+const frutas = ['Maçã', 'Banana', 'Laranja', 'Limão', 'Abacaxi'];
+
+frutas.splice(2, 2, 'Kiwi', 'Pêssego');
+
+console.log(frutas);
+
+// 4
+
+const menuPrincipal = ['Macarrão', 'Bifé', 'Pastel'];
+const menuDeSobremesas = ['Sorvete', 'Bolo', 'Doce']
+
+const menuCompleto = menuPrincipal.concat(menuDeSobremesas);
+
+console.log('Veja o menu completo:', menuCompleto );
+
+// 5
+
+let matriz = [];
+let valorInicial = 1;
+
+for (let i = 0; i < 3; i++) {
+  let linha = [];
+  for (let j = 0; j < 3; j++) {
+    linha.push(valorInicial++);
+  }
+  matriz.push(linha);
+}
+
+console.log('Matriz de duas dimensões:');
+console.log(matriz);
+matriz.forEach(row => console.log(row)); // visualização em linhas e colunas
+
+// 6
+
+console.log('Elemento na segunda linha e terceira coluna:', matriz[1][2]);
+
+// 7
+
+matriz[2][1] = 15;
+console.log('Matriz após adição de elemento:', matriz);
---
+++ Aula10.js
@@ -0,0 +1,14 @@
+const alunos = ['João','Pedro','Paula','Vitor']
+const notas = [10,5,9,2]
+
+const lista =[alunos,notas]
+
+function NotaDoAluno(alunos){
+    if (lista[0].includes(alunos)){
+        const indice = lista[0].indexOf(alunos)
+        const mediaAluno = lista[1][indice]
+        return console.log(`O Aluno ${alunos}, tem a nota: ${mediaAluno}`)
+    } else { console.log(`Aluno ${alunos} não encontrado.`)}
+}
+
+NotaDoAluno("Paula")
